898ac54b51a9ffdd0a19477231b0f0e3256788fe
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-05-06 20:29:36 -0300

    exe.list001

--- Aulas/Exe.Lista.py
+++ Aulas/Exe.Lista.py
@@ -5,6 +5,8 @@ print(mercado[0], mercado[1], mercado[2], mercado[3])
 print(len(mercado))
 print(mercado)
 
+#segunda ativade
+
 mercado_preco = ['3.5', '5', '4', '5.5', '8.0']
 print(mercado_preco[0])
 
@@ -13,7 +15,10 @@ print('você removeu o item: frango')
 
 total = float(mercado_preco[0]) + float(mercado_preco[1]) + float(mercado_preco[2]) + float(mercado_preco[3]) + float(mercado_preco[4])
 print ('o valor total da compra é: R$', total)
- 
+
+if total >= 20:
+    print('o valor total da compra é: R$', total, 'você tem direito a um desconto de 5%')
+     
 desconto = total * 0.95
 print('o valor total da compra com desconto é: R$', desconto)
 
