07acded9f328fc7dc757ed899a04acd7d5df4f31
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-05-11 21:33:43 -0300

    007

--- Aulas/checkout.py
+++ Aulas/checkout.py
@@ -1,9 +1,11 @@
+
+
 produtos = ["camiseta", "calça", "par meias", "boné", "touca"]
 precos = [40.00, 80.00, 15.00, 20.00, 20.00]
 quantidades = [4, 4, 5, 6, 7]
 subtotais = []
 
-print(f'O produto {produtos[0]} custa R$ custa R${precos[0]}')
+print(f'O produto {produtos[0]} custa R${precos[0]}')
 
 for indice, produto in enumerate(produtos):
     preco = precos[indice]
@@ -26,4 +28,33 @@ print(mensagem)
 
 
 print(f"O total da compra deu: R${sum(subtotais)}")
-prin
+
+
+
+
+produtos = ["camiseta", "calça", "par meias", "boné", "touca"]
+precos = [40.00, 80.00, 15.00, 20.00, 20.00]
+quantidades = [2, 1, 2, 1, 1]
+subtotais = []
+# ANTES, FARIA ASSIM PARA PEGAR O PRODUTO E PREÇO:
+
+print(f"O produto {produtos[0]} custa R${precos[0]}.")
+
+for indice, produto in enumerate(produtos):
+    preco = precos[indice] # preco = precos[0]
+    quantidade = quantidades[indice]
+    subtotal = quantidade * preco
+    subtotais.append(subtotal)
+
+    mensagem = f"""
+    -----------------------------------
+    Produto: {produto}
+    Quantidade: {quantidade}
+    Valor unitário: {preco}
+    Subtotal: {subtotal}
+    -----------------------------------
+    """
+
+    print(mensagem)
+
+print(f"O total da compra deu: R${sum(subtotais)}")
