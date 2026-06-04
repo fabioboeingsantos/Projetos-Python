8f6a75e32896349e74369d6089e55a8379eb4326
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-06-03 23:39:38 -0300

    002.2

---
+++ Guanabara/tabuada_simples.py
@@ -0,0 +1,10 @@
+'''Faca um programa que leia o numero inteiro e mostre na tela a sua tabuada'''
+
+numero = int(input('Digite o numero da tabuada: '))
+print("-" * 15)
+print("{} x {} = {} ".format(numero, 1, numero*1))
+print('{} x {} = {} '.format(numero, 2, numero*2))
+print ('{} x {} = {}'.format(numero, 3, numero*3))
+print('{} x {} = {}'.format(numero, 4, numero*4))
+print('{} x {} = {}'.format(numero, 5, numero*5))
+print("-" * 15)
