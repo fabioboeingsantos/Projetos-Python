3a15aa7931277771f0cbaec1619ac06f5f3b6f45
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-06-10 21:59:39 -0300

    003.1

--- Exercicios_senac/classe001.py
+++ Exercicios_senac/classe001.py
@@ -1,16 +1,22 @@
 class Produto():
-    def __unit__(self, nome: str, preco: float, qtd: int):
+    def __init__(self, nome: str, preco: float, qtd: int):
         self.nome = nome
         self.preco = preco
         self.qtd = qtd
 
     def exibir(self):
-        print(f'produto {self.nome}, preco{self.preco}, qtd{self.qtd}')
+        print(f'produto: {self.nome}, preco R$:{self.preco:.2f}, qtd {self.qtd} unidades.')
 
-produto_01 = Produto("arroz", 10.0, 15)
+#primera forma de fazer
+produto_01 = Produto("Arroz", 10.00, 15)
 
-produto_02 = Produto(nome="feijao", preco=9.0, qtd=9)
+#segunda forma de fazer
+produto_02 = Produto(nome="feijao", preco=9.0, qtd=9) #esse igual passa a quantidade ja definida.
 
 
 
 produto_01.exibir()
+
+
+
+
---
+++ Exercicios_senac/classe003.py
@@ -0,0 +1,37 @@
+'''
+A classe Aluno
+- Crie a classe alunos com nome e nota
+- Crie 2 alunos com valores diferentes
+- imprima o nome e a nota de cada um
+
+2 parte
+- reaproveite a classe aluno, crie o metodo 'situacao()'
+- se a nota for >=6, mostre aprovado
+- Caso contrario, mostre reprovado
+- teste com os 2 alunos diferentes
+
+'''
+
+
+class Alunos():
+    def __init__(self, nome: str, nota: float):
+        self.nome = nome
+        self.nota = nota
+        
+    def situacao(self):
+        if Alunos >= 6:
+            print('aprovado')
+        else:
+            print('reprovado')
+
+
+    def exibir(self):
+        print(f'Nome: {self.nome}, Nota {self.nota}, situacao {self.situacao}')
+
+aluno01 = Alunos("fabio", 6.0)
+aluno02 = Alunos("fabia", 8.0)
+
+print('='*30 )
+aluno01.exibir()
+print('='*30 )
+
