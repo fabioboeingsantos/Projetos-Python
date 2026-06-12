09ae3426289937acffa8689618a9dfdbcc87aeed
Fabio Boeing Santos <fabio.boeing@yahoo.com.br>
2026-06-12 00:23:12 -0300

    004.1

--- Exercicios_senac/diario_classe.py
+++ Exercicios_senac/diario_classe.py
@@ -1,20 +1,29 @@
+
 class Alunos():
     def __init__(self, nome: str, notas: []):
         self.nome = nome
         self.notas = notas
         
     def exibir(self):        
-        print(f'Nome: {self.nome}, media: {self.media} ')
+        print(f'Nome: {self.nome}, notas: {self.notas} ')
 
-    #def de somar, logica de somar, por dizer se esta aprovado ou reprovado    
+   
     def situacao(self):
-        sum(self.notas)
-        media = self.somar() / len(self.notas)  
+        if not self.notas:
+            print(f'O aluno {self.nome}, ainda nao possui notas cadastradas. ')
+            return                                          #usado para interromper, faz a checagem e voltar ao menu... nao esquecer desse ***...
+
+        soma_das_notas = sum(self.notas)
+        media = soma_das_notas / len(self.notas)  
+
+
         if media >= 7:
-            print("Aprovado") 
+            print(f'A media foi de {media:.2f} - Voce esta Aprovado') 
                 
         else:
-            print('Reprovado')
+            print(f'A media foi de {media:.2f} - Voce esta Reprovado')
+
+
 
 def menu_simples():
     print('='*30)
@@ -29,35 +38,58 @@ def menu_simples():
 def cadastrar_alunos():
     print('\nCadastrando ALuno')
     nome = input('Digite seu nome: ')
-    novo_aluno = Alunos(nome)
+    novo_aluno = Alunos(nome,[])                            # adicinar uma lista vazia aqui, em referencia a classe, nome, notas []
     lista_de_alunos.append(novo_aluno)
     print(f'{nome} cadastrado com sucesso. ')
 
-def cadastrar_notas():
+def lancar_notas():
+    print('='*30)
     print('\nCadastrando Notas')
-    indice = float(input('Digite sua nota: '))
+
+    if not lista_de_alunos:
+        print('Nenhum aluno cadastrado')
+        return                                  #usado para interromper e voltar ao menu
+
+
+    for posicao, aluno in enumerate(lista_de_alunos):           #busca na lista a posicao do aluno
+        print(f'{posicao + 1} - {aluno.nome}')
+
+    num_aluno_escolhido = int(input('Adicione o aluno de acordo com numero da lista: '))    #input com a selecao do enumerate
+
+    indice = num_aluno_escolhido -1
+    aluno = lista_de_alunos[indice]
+
+    nota_adicionada = float(input('Adicione sua nota: '))    
+    aluno.notas.append(nota_adicionada)    
+
+    print(f'{aluno.nome} - {nota_adicionada:.2f} adicionada com sucesso. ')
     
-    
+
+
+def ver_situacao():
+    if not lista_de_alunos:
+        print('Nenhum aluno cadastrado: ')
+        return                                              #usado para interromper, faz a checagem e voltar ao menu...
+
+    for posicao, aluno in enumerate(lista_de_alunos):
+        print(f'{posicao + 1} - {aluno.nome}')
+
+    num_aluno_escolhido = int(input('Digite o aluno de acordo com o numero da lista: '))
+
+    indice = num_aluno_escolhido -1
+    aluno = lista_de_alunos[indice]                         #Nao posso esquecer desse detalhe de definir o indice de inicio do codigo
+
+    aluno.situacao()
 
 
 def listar_alunos():
-    print(f'\n{'Listando Aluno':^30}')
     if not lista_de_alunos:
-        print('Nao esta na lista. ')
-        
-    else:
-        for aluno in lista_de_alunos:
-           aluno.exibir()
-    print('='*30)
-    
-def situacao(self):
-        sum(self.notas)
-        media = self.somar() / len(self.notas)  
-        if media >= 7:
-            print("Aprovado ") 
-                
-        else:
-            print('Reprovado')
+        print('Nenhum aluno cadastrado.')
+        return
+
+
+    for aluno in lista_de_alunos:
+        aluno.exibir()
 
 lista_de_alunos = []
 
@@ -73,11 +105,10 @@ while True:
         cadastrar_alunos()
     
     elif Opcao == '2':
-        cadastrar_notas()
+        lancar_notas()
 
     elif Opcao == '3':
-        situacao()  
-
+        ver_situacao()  
 
     elif Opcao == '4':
         listar_alunos()
--- Pratica/A.A TESTES.py
+++ Pratica/A.A TESTES.py
@@ -1,2 +1,11 @@
 
 
+def situacao(self):
+    soma_notas = sum(self.notas)
+    media = soma_notas / len(self.notas)  
+    
+    if media >= 7:
+            print("Aprovado ") 
+                
+    else:
+            print('Reprovado')
